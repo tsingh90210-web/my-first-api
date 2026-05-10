71c436a9fa9a5b1309c4b34b1df5dede7180f846
Tajinder Singh <tajinder.singh@skillsoft.com>
2026-05-10 21:05:00 +0100

    WIP -fixing routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module'; // Import our new module
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule], // Register the Prisma module
+  imports: [PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
